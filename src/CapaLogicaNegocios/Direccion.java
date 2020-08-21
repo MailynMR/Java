c75b601cd0e05ce91b11a8c9010004d531c5c69c
MailynMR <madrigalramirezmailyn@gmail.com>
2020-08-20 19:25:26 -0600

    Actualizacion Clase direccion mantenimiento

--- src/CapaLogicaNegocios/Direccion.java
+++ src/CapaLogicaNegocios/Direccion.java
@@ -10,4 +10,52 @@ import java.io.Serializable;
 
 public class Direccion implements Serializable {
     
+    String provincia;
+    String canton;
+    String distrito;
+    String señas;
+
+    public Direccion(String provincia, String canton, String distrito, String señas) {
+        this.provincia = provincia;
+        this.canton = canton;
+        this.distrito = distrito;
+        this.señas = señas;
+    }
+
+    public String getSeñas() {
+        return señas;
+    }
+
+    public void setSeñas(String señas) {
+        this.señas = señas;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getCanton() {
+        return canton;
+    }
+
+    public void setCanton(String canton) {
+        this.canton = canton;
+    }
+
+    public String getDistrito() {
+        return distrito;
+    }
+
+    public void setDistrito(String distrito) {
+        this.distrito = distrito;
+    }
+    
+    
+    
+    
+    
 }
